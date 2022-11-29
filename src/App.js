955e49b684e2e8b6792d96cbece7c7aa1b1c0586
connorwarme <connor.warme@gmail.com>
2022-11-29 16:31:49 -0700

    Simple footer, getting a feel for React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './styles/App.css';
 import Header from './components/Header';
 import Card from './components/Card';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Card />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,24 @@
 import React from "react";
+import '../styles/Footer.css';
 
 class Footer extends React.Component {
     render() {
         return (
-
+            <div className="footer">
+                <div className="footerContent">
+                    <div className="linkContainer">
+                        <a href="https://github.com/connorwarme">
+                            <img alt="GitHub Profile"></img>
+                        </a>
+                        <a href="https://www.linkedin.com/in/connor-warme-103a09167">
+                            <img alt="LinkedIn Profile" target="_blank"></img>
+                        </a>
+                    </div>
+                    <div className="textContainer">
+                        <div className="footerText">Peregrinning Productions</div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }

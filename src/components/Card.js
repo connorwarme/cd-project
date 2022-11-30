d28dd6871e13097544dfe3f277876457101641da
connorwarme <connor.warme@gmail.com>
2022-11-29 17:14:55 -0700

    Begin building card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,25 @@
 import React from "react";
 
 class Card extends React.Component {
+    constructor(props) {
+        super(props)
+        // this.state = {
+        //     name: 'Connor',
+        //     address: 'Long Branch',
+        //     city: '',
+        //     state: '',
+        //     zip: '',
+        // }
+    }
     render() {
+        const { fullname, address, city, state, zip } = this.props;
         return (
             <div className="card">
-                <div>Name</div>
-                <div>Address</div>
-                <div>City</div>
-                <div>State</div>
-                <div>Zip</div>
+                <input type="text" placeholder="Name" />
+                <input type="text" placeholder="Address" />
+                <input type="text" placeholder="City" />
+                <input type="text" placeholder="State" />
+                <input type="text" placeholder="Zip" />
             </div>
         )
     }

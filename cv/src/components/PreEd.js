c3d84796b3e7a4b618f4b9866b4fc6f2604fb417
connorwarme <connor.warme@gmail.com>
2022-12-08 14:06:52 -0700

    Modify preview for Work and Ed so slash and dash only appear when relevant content included

--- cv/src/components/PreEd.js
+++ cv/src/components/PreEd.js
@@ -2,6 +2,22 @@ import React from "react";
 
 // preview education unit
 class PreEd extends React.Component {
+    constructor(props) {
+        super(props)
+        this.createSlash = this.createSlash.bind(this)
+        this.createDash = this.createDash.bind(this)
+    }
+    createSlash(input) {
+        if (input.location.length > 0 && (input.from.length > 0 || input.to.length > 0)) {
+            return <div> | </div>
+        }
+    }
+    createDash(input) {
+        if (input.from.length > 0 && input.to.length > 0) {
+            return <div>{input.from} - {input.to}</div>
+        }
+        return <div>{input.from} {input.to}</div>
+    }
     render() {
         const { data } = this.props;
         return (
@@ -10,8 +26,8 @@ class PreEd extends React.Component {
                     <div className="preview-edu-school">{data.school}</div>
                     <div className="preview-edu-info">
                         <div>{data.location}</div>
-                        <div>|</div>
-                        <div>{data.from} - {data.to}</div>
+                        <div>{this.createSlash(data)}</div>
+                        <div>{this.createDash(data)}</div>
                     </div>
                 </div>
                 <div className="preview-edu-degree">{data.degree}</div>
--- cv/src/components/PreWork.js
+++ cv/src/components/PreWork.js
@@ -2,6 +2,22 @@ import React from "react";
 
 // preview work experience unit
 class PreWork extends React.Component {
+    constructor(props) {
+        super(props)
+        this.createSlash = this.createSlash.bind(this)
+        this.createDash = this.createDash.bind(this)
+    }
+    createSlash(input) {
+        if (input.company.length > 0 && (input.start.length > 0 || input.finish.length > 0)) {
+            return <div> | </div>
+        }
+    }
+    createDash(input) {
+        if (input.start.length > 0 && input.finish.length > 0) {
+            return <div>{input.start} - {input.finish}</div>
+        }
+        return <div>{input.start} {input.finish}</div>
+    }
     render() {
         const { data } = this.props;
         return (
@@ -10,8 +26,8 @@ class PreWork extends React.Component {
                     <div className="preview-wu-title">{data.title}</div>
                     <div className="preview-wu-info">
                         <div>{data.company}</div>
-                        <div>|</div>
-                        <div>{data.start} - {data.finish}</div>
+                        <div>{this.createSlash(data)}</div>
+                        <div>{this.createDash(data)}</div>
                     </div>
                 </div>
                 <div className="preview-wu-details">{data.details}</div>
